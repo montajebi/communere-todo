28eed493f8d162e6757b60c967c093fa7d688ace
Ali Montajebi <montajebi.ali@gmail.com>
2022-02-22 17:09:42 +0330

    FIX: show error just when touched

--- src/components/design-system/data-entry/Input.tsx
+++ src/components/design-system/data-entry/Input.tsx
@@ -1,5 +1,5 @@
 import cx from 'classnames';
-import { HTMLInputTypeAttribute } from 'react';
+import { HTMLInputTypeAttribute, useMemo, useState } from 'react';
 
 interface InputProps {
   name: string;
@@ -22,9 +22,13 @@ const Input = (props: InputProps) => {
     onChange,
   } = props;
 
+  const [touched, setTouched] = useState(false);
+
+  const hasError = useMemo(() => error && touched, [error, touched]);
+
   const inputClassName = cx(
     'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline',
-    { 'border-red-500': error },
+    { 'border-red-500': hasError },
   );
 
   const lableComponent = label ? (
@@ -45,10 +49,12 @@ const Input = (props: InputProps) => {
       placeholder={placeholder}
       value={value}
       onChange={onChange}
+      onFocus={() => setTouched(true)}
+      onBlur={() => setTouched(false)}
     />
   );
 
-  const errorComponent = error ? (
+  const errorComponent = hasError ? (
     <p className="text-red-500 text-xs italic">{error}</p>
   ) : null;
 
