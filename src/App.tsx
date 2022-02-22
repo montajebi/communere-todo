a820cb909ca112264152490e69a3062fd606f41c
Ali Montajebi <montajebi.ali@gmail.com>
2022-02-22 17:09:54 +0330

    ADD: todos page and use it in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,7 @@
-import usePresistState from './hooks/usePresistState';
-import TodoAdder, { Todo } from './components/todos/TodoAdder';
-import { Flex, Grid, Margin } from './components/design-system';
+import TodosPage from './pages/todos/TodosPage';
 
 const App = () => {
-  const [todos, setTodos] = usePresistState('todos', []);
-
-  const todoItems = todos.map((todo: Todo) => {
-    return (
-      <div className="border-solid rounded-md border-2 py-4 px-4 border-gray-20 text-gray-700">
-        <Flex direction="row" justify="between">
-          <div>{todo.title}</div>
-        </Flex>
-      </div>
-    );
-  });
-
-  return (
-    <div>
-      <Margin top="10" />
-      <Grid.Container cols="6" gap="4">
-        <Grid.Item start="2" span="4">
-          <TodoAdder onAddTodo={(newTodo) => setTodos([newTodo, ...todos])} />
-
-          <div className="mt-5"></div>
-          <Flex gap="4" direction="col" justify="center">
-            {todoItems}
-          </Flex>
-        </Grid.Item>
-      </Grid.Container>
-    </div>
-  );
+  return <TodosPage />;
 };
 
 export default App;
---
+++ src/pages/todos/TodosPage.tsx
@@ -0,0 +1,66 @@
+import usePresistState from '../../hooks/usePresistState';
+import TodoAdder, { Todo } from './components/TodoAdder';
+import TodoCard from './components/TodoCard';
+import { Flex, Grid, Margin } from '../../components/design-system';
+import { useCallback } from 'react';
+
+const TodosPage = () => {
+  const [todos, setTodos] = usePresistState('todos', []);
+
+  console.log(todos);
+
+  const deleteTodo = useCallback(
+    (id) => {
+      const newList = todos.filter((todo: Todo) => todo.id !== id);
+
+      setTodos([...newList]);
+    },
+    [todos, setTodos],
+  );
+
+  const doneTodo = useCallback(
+    (id) => {
+      const newList = todos.map((todo: Todo) => {
+        if (todo.id === id) {
+          return { ...todo, done: !todo.done };
+        }
+
+        return todo;
+      });
+
+      setTodos([...newList]);
+    },
+    [todos, setTodos],
+  );
+
+  const todoItems = todos.map((todo: Todo) => {
+    return (
+      <TodoCard
+        key={todo.id}
+        id={todo.id}
+        title={todo.title}
+        createdDate={new Date()}
+        done={todo.done}
+        onDeleteClick={() => deleteTodo(todo.id)}
+        onDoneClick={() => doneTodo(todo.id)}
+      />
+    );
+  });
+
+  return (
+    <div>
+      <Margin top="10" />
+      <Grid.Container cols="6" gap="4">
+        <Grid.Item start="2" span="4">
+          <Flex gap="4" direction="col">
+            <TodoAdder onAddTodo={(newTodo) => setTodos([newTodo, ...todos])} />
+
+            {todoItems}
+          </Flex>
+        </Grid.Item>
+      </Grid.Container>
+    </div>
+  );
+};
+
+export default TodosPage;
