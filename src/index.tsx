676ca17a48c61e87596e0bf6e9a9d7e5d9852b04
Ali Montajebi <montajebi.ali@gmail.com>
2022-02-23 10:08:05 +0330

    FIX: tailwind css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {
-      animation: {
-        bounce200: 'bounce 1s infinite 200ms',
-        bounce400: 'bounce 1s infinite 400ms',
-      },
-    },
+      extend: {},
   },
   plugins: [],
-};
+}
